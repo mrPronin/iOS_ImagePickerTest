92d37d8682e72530e704bbd5f49b63fdabbc5a20
Pronin Alexander <pronin.alx@gmail.com>
2014-04-28 18:00:26 +0300

    First commit

--- ImagePickerTest/RITViewController.m
+++ ImagePickerTest/RITViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "RITViewController.h"
+#import <MobileCoreServices/UTCoreTypes.h>
 
 @interface RITViewController ()
+
+static UIImage *shrinkImage(UIImage *original, CGSize size);
+- (void) updateDisplay;
+- (void) getMediaFromSource:(UIImagePickerControllerSourceType) sourceType;
 
 @end
 
@@ -17,13 +22,55 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        self.takePictureButton.hidden = YES;
+    }
+    self.imageFrame = self.imageView.frame;
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    
+    [super viewDidAppear:animated];
+    [self updateDisplay];
+}
+
+- (void) dealloc {
+    
+    self.moviePlayercontroller = nil;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (IBAction)shootPictureOrVideo:(UIButton *)sender {
+    
+    [self getMediaFromSource:UIImagePickerControllerSourceTypeCamera];
+}
+
+- (IBAction)selectExistingPictureOrVideo:(UIButton *)sender {
+    
+    [self getMediaFromSource:UIImagePickerControllerSourceTypePhotoLibrary];
+}
+
+#pragma mark - UIImagePickerController delegate mathods
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    self.lastChosenMediaType = [info objectForKey:UIImagePickerControllerMediaType];
+    if ([self.lastChosenMediaType isEqualToString:(NSString*)kUTTypeImage]) {
+        UIImage *chosenImage = [info objectForKey:UIImagePickerControllerEditedImage];
+        UIImage *shrunkedImage = shrunkedImage(chosenImage, self.imageFrame.size);
+        self.image = shrunkedImage;
+    } else {
+        
+    }
 }
 
 @end
